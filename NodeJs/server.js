623ab89ca29b30746a237e2bc0576ca03bb4592a
pk381 <kumarprabhat0023@gmail.com>
2023-07-22 08:42:27 +0530

    sending response for different requests

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,8 +1,33 @@
 const http = require('http');
 
+// console.log("running");
+
 const server = http.createServer((req, res)=>{
 
-    console.log("Prabhat Kumar");
+    console.log("requests");
+
+    if(req.url === '/home'){
+        // console.log("home");
+        res.write('<html><head><title>Document</title></head>');
+        res.write('<body><h1>Welcome Home</h1></body></html>');
+        res.end();
+    }
+    else if(req.url === '/about'){
+
+        // console.log("about");
+        res.write('<html><head><title>Document</title></head>');
+        res.write('<body><h1>Welcome to About Us Page</h1></body></html>');
+        res.end();
+
+    }
+    else if(req.url === '/node'){
+
+        // console.log("node");
+        res.write('<html><head><title>Document</title></head>');
+        res.write('<body><h1>Welcome to Node Js Project</h1></body></html>');
+        res.end();
+
+    }
     
 });
 
