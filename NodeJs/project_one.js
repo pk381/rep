bd0cc498640d173fb61a1b69e3f07bd6d24e52d1
pk381 <kumarprabhat0023@gmail.com>
2023-07-22 14:01:02 +0530

    exporting and making module

--- NodeJs/project_one.js
+++ NodeJs/project_one.js
@@ -1,51 +1,10 @@
 const http = require('http');
 const fs = require('fs');
-const { buffer } = require('stream/consumers');
+
+const route = require('./routes');
 
 console.log("running");
 
-const server = http.createServer((req, res)=>{
-
-    const url = req.url;
-    const method = req.method;
-
-    if(url === '/'){
-
-        fs.readFile('message.txt', 'UTF-8',(err, data)=>{
-            console.log("data = ", data);
-
-            console.log("gi");
-
-            res.write('<html><head><title>Document</title></head>');
-            res.write(`<body><h1>${data}</h1><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body></html>`);
-            return res.end();
-        });
-
-    }
-    
-    if(url === '/message' && method === 'POST'){
-
-        console.log("in message");
-
-        const body = [];
-        req.on('data', (chunk)=>{
-            body.push(chunk);
-        });
-
-        req.on('end', ()=>{
-            const messsage = Buffer.concat(body).toString().split("=")[1];
-            // console.log(messsage);
-            fs.writeFileSync("message.txt", messsage);
-
-        });
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-
-    }
-    
-    
-});
+const server = http.createServer(route);
 
 server.listen(4000);
---
+++ NodeJs/routes.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+
+const requestHandler = (req, res)=>{
+
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/'){
+
+        fs.readFile('message.txt', 'UTF-8',(err, data)=>{
+            console.log("data = ", data);
+
+            console.log("gi");
+
+            res.write('<html><head><title>Document</title></head>');
+            res.write(`<body><h1>${data}</h1><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body></html>`);
+            return res.end();
+        });
+
+    }
+    
+    if(url === '/message' && method === 'POST'){
+
+        console.log("in message");
+
+        const body = [];
+        req.on('data', (chunk)=>{
+            body.push(chunk);
+        });
+
+        req.on('end', ()=>{
+            const messsage = Buffer.concat(body).toString().split("=")[1];
+            // console.log(messsage);
+            fs.writeFileSync("message.txt", messsage);
+
+        });
+
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+
+    }
+    
+    
+}
+
+module.exports = requestHandler;
