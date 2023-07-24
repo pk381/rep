2f911bffffdcd17580f60f65fa0b26ae318e13e9
pk381 <kumarprabhat0023@gmail.com>
2023-07-24 09:40:36 +0530

    adding new route and parsing body

--- NodeJs/project_one.js
+++ NodeJs/project_one.js
@@ -1,11 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use((req, res, next)=>{
-
-    res.send({one: "one"});
-});
+const bodyParser = require("body-parser");
 
 console.log("running");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  console.log("product-form");
+  res.send(
+    '<form action="product" method ="POST"> Title: <input type="text" name="title"> Size: <input type="text" name="size"> <button type="submit">send</button></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express JS</h1>");
+});
 
 app.listen(4000);
